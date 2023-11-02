58dbd8c2fa3e31b83d250b47d9780e9e642bda50
chuxiwen <1609185446@qq.com>
2023-11-02 11:04:50 +0800

    定义Client结构

--- day2-client/client.go
+++ day2-client/client.go
@@ -1,4 +1,11 @@
-package day2_client
+package gorpc
+
+import (
+	"errors"
+	"gorpc/codec"
+	"io"
+	"sync"
+)
 
 type Call struct {
 	Seq           uint64
@@ -11,4 +18,37 @@ type Call struct {
 
 func (call *Call) done() {
 	call.Done <- call
+}
+
+type Client struct {
+	cc       codec.Codec // 编码器
+	opt      *Option
+	sending  sync.Mutex   // 互斥锁
+	header   codec.Header // 请求头
+	mu       sync.Mutex
+	seq      uint64 // 请求编号
+	pending  map[uint64]*Call
+	closing  bool // 用户主动关闭
+	shutdown bool // 有错误发生
+}
+
+var _ io.Closer = (*Client)(nil)
+
+var ErrShutDown = errors.New("connection is shut down")
+
+// Close 关闭链接
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing {
+		return ErrShutDown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
 }
