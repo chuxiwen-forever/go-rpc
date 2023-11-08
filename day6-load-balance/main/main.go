69f2ec2215d11bf2e4f21faed2a1f7eaf6037f2a
chuxiwen <1609185446@qq.com>
2023-11-08 10:08:27 +0800

    初始化第六天工程

---
+++ day6-load-balance/main/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"gorpc"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Foo int
+
+type Args struct {
+	Num1, Num2 int
+}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addr chan string) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":9999")
+	_ = gorpc.Register(&foo)
+	gorpc.HandleHTTP()
+	addr <- l.Addr().String()
+	_ = http.Serve(l, nil)
+}
+
+func call(addr chan string) {
+	client, _ := gorpc.DialHTTP("tcp", <-addr)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{
+				Num1: i,
+				Num2: i * i,
+			}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
+}
