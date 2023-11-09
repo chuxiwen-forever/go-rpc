805b008ba1030bea51746629ffd7e9fdddd85734
chuxiwen <1609185446@qq.com>
2023-11-09 11:24:11 +0800

    提供心跳机制

--- day7-registry/registry/registry.go
+++ day7-registry/registry/registry.go
@@ -84,3 +84,30 @@ func (r *GoRegistry) HandleHTTP(registryPath string) {
 func HandleHTTP() {
 	DefaultGoRegister.HandleHTTP(defaultPath)
 }
+
+func Heartbeat(registry, addr string, duration time.Duration) {
+	if duration == 0 {
+		duration = defaultTimeout - time.Duration(1)*time.Minute
+	}
+	var err error
+	err = sendHeartbeat(registry, addr)
+	go func() {
+		t := time.NewTicker(duration)
+		for err == nil {
+			<-t.C
+			err = sendHeartbeat(registry, addr)
+		}
+	}()
+}
+
+func sendHeartbeat(registry string, addr string) error {
+	log.Println(addr, "send heart beat to registry", registry)
+	httpClient := &http.Client{}
+	req, _ := http.NewRequest("POST", registry, nil)
+	req.Header.Set("X-Gorpc-Server", addr)
+	if _, err := httpClient.Do(req); err != nil {
+		log.Println("rpc server: heart beat err:", err)
+		return err
+	}
+	return nil
+}
